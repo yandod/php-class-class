77b5f30093d7d614a730410cbadeea0b441fb3d9
Yusuke Ando <yandod@gmail.com>
2012-03-19 11:56:03 +0900

    step03

--- src/step03.php
+++ src/step03.php
@@ -2,11 +2,12 @@
 $out = '1000';
 function hoge() {
   $in = 'ABCD';
-  return date('Y-m-d');
+  //echo $out; // エラー
+  //echo $etc; // エラー
 }
-echo hoge(); // ‘2012-03-21’ などと表示
-echo Hoge();
-echo HOGE();
-echo hogE();
-$a = 'hoge';
-echo $a();
+function fuga() {
+  $etc = 'XYZ';
+}
+hoge();
+hoge();
+//echo $in; //エラー
